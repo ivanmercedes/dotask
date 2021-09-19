c1e9309e4e19366f665d0c3507543a2c6be43999
Ivan Mercedes <ivanmercede@gmail.com>
2021-09-19 14:00:29 -0400

    Fix .env

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("dotask", "root", "", {
-  host: "localhost",
-  dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
-  port: "3306",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_ENGINE /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+  port: process.env.DB_PORT,
   operatorsAliases: "0",
   define: {
     timestamps: "0",
